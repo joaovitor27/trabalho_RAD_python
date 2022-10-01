de6c23ec1e99a6ec142a86ee8255cbda999ba48f
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2022-10-01 10:23:36 -0300

    criando funções para criar tabelas no banco de dados

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def get_db():
-    conn = sqlite3.connect('./data/chatbot.sqlite')
+    conn = sqlite3.connect('database.sqlite')
     return conn
 
 
@@ -11,6 +11,7 @@ def create_table(sql: str):
     cursor = conn.cursor()
     cursor.execute(sql)
     conn.close()
+    return "Table created success"
 
 
 def query_db(query: str, args: tuple = (), one: bool = False):
@@ -29,13 +30,31 @@ def insert_db(query: str, args: tuple = ()):
     return cur.rowcount
 
 
-def get_client_db(establishment, customer):
-    result = query_db('SELECT name FROM contacts WHERE number=? AND owner=? LIMIT 1',
-                      (str(customer.phone), establishment.owner), True)
+# def get_client_db(establishment, customer):
+#     result = query_db('SELECT name FROM contacts WHERE number=? AND owner=? LIMIT 1',
+#                       (str(customer.phone), establishment.owner), True)
+#     return result
+#
+#
+# def insert_client(customer, establishment):
+#     result = insert_db('INSERT INTO contacts (owner, number, name) VALUES (?, ?, ?)',
+#                        (str(establishment.owner), str(customer.phone), str(customer.name)))
+#     return result
+
+
+def create_table_pessoa():
+    result = create_table('CREATE TABLE IF NOT EXISTS people (id_person INTEGER AUTO_INCREMENT, cpf VARCHAR(14), '
+                          'first_name TEXT, middle_name TEXT, last_name TEXT, age integer,conta INTEGER, '
+                          'FOREIGN KEY (conta) references conta (number_conta))')
     return result
 
 
-def insert_client(customer, establishment):
-    result = insert_db('INSERT INTO contacts (owner, number, name) VALUES (?, ?, ?)',
-                       (str(establishment.owner), str(customer.phone), str(customer.name)))
+def create_table_conta():
+    result = create_table('CREATE TABLE IF NOT EXISTS conta (id_conta INTEGER AUTO_INCREMENT, agency INTEGER, '
+                          'number_conta INTEGER, saldo REAL, gerente TEXT, titular TEXT)')
     return result
+
+
+if __name__ == '__main__':
+    print(create_table_conta())
+    print(create_table_pessoa())
