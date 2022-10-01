1e87eb60bfb8bdfd228c2aa00776083b3ff658cd
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2022-10-01 16:09:18 -0300

    criando put, delete e select do banco de dados

--- database.py
+++ database.py
@@ -30,9 +30,27 @@ def insert_db(query: str, args: tuple = ()):
     return 'inserido com sucesso'
 
 
+def delete_db(query: str, args: tuple = ()):
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute(query, args)
+    conn.commit()
+    conn.close()
+    return 'Excluído com sucesso'
+
+
+def put_db(query: str, args: tuple = ()):
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute(query, args)
+    conn.commit()
+    conn.close()
+    return 'Dado editado com sucesso'
+
+
 def create_table_pessoa():
     result = create_table('CREATE TABLE IF NOT EXISTS people (id_person INTEGER PRIMARY KEY AUTOINCREMENT, '
-                          'cpf VARCHAR(14) UNIQUE, first_name TEXT, middle_name TEXT, last_name TEXT, age integer,'
+                          'cpf VARCHAR(11) UNIQUE, first_name TEXT, middle_name TEXT, last_name TEXT, age integer,'
                           ' conta INTEGER UNIQUE, FOREIGN KEY (conta) references conta (number_conta))')
     return result
 
@@ -56,32 +74,52 @@ def insert_pessoa(cpf: str, first_name: str, middle_name: str, last_name: str, a
     return result
 
 
-def delete_conta():
-    pass
+def format_cpf(cpf: str):
+    cpf = cpf.replace('.', '')
+    cpf = cpf.replace('-', '')
+    return cpf
 
 
-def delete_pessoa():
-    pass
+def delete_conta(number_conta: int):
+    result = delete_db('DELETE FROM conta WHERE number_conta=?', (number_conta, ))
+    return result
 
 
-def put_conta():
-    pass
+def delete_pessoa(cpf: str):
+    cpf = format_cpf(cpf)
+    result = delete_db('DELETE FROM people WHERE cpf=?', (cpf,))
+    return result
 
 
-def put_pessoa():
-    pass
+def put_conta(number_conta: int, saldo: float, agency: int, gerente: str, titular: str):
+    result = put_db('UPDATE conta SET saldo = ?, agency = ?, gerente = ?, titular = ? WHERE number_conta=?',
+                    (saldo, agency, gerente, titular, number_conta))
+    return result
 
 
-def get_pessoa():
-    pass
+def put_pessoa(cpf: str, first_name: str, middle_name: str, last_name: str, age: int, conta: int):
+    cpf = format_cpf(cpf)
+    result = put_db('UPDATE people SET first_name=?, middle_name=?, last_name=?, age=?, conta=? WHERE cpf=?',
+                    (first_name, middle_name, last_name, age, conta, cpf))
+    return result
 
 
-def get_conta():
-    pass
+def get_pessoa(cpf: str):
+    cpf = format_cpf(cpf)
+    result = query_db('SELECT * FROM people WHERE cpf=?', (cpf, ))
+    return result
 
 
-def get_pessoa_conta():
-    pass
+def get_conta(number_conta: int):
+    result = query_db('SELECT * FROM conta WHERE number_conta=?', (number_conta, ))
+    return result
+
+
+def get_pessoa_conta(cpf):
+    cpf = format_cpf(cpf)
+    result = query_db('SELECT * FROM people left join conta c on c.number_conta = people.conta '
+                      'WHERE cpf=?', (cpf, ))
+    return result
 
 
 if __name__ == '__main__':
